d2dd63429bba0cbb8d6cb1e210e60274061cad9a
heiner <heiner.andres@outlook.com>
2025-05-16 17:19:43 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+try{
+    app.use(express.json());
+    app.use(cors());
+
+    app.use(express.static('public'));
+
+    app.use('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+    });
+
+    app.use('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+    });
+
+
+    app.listen( 8080, () => {
+        console.log("Servidor listo en el puerto 8080");
+    });
+}
+catch(error){
+    console.log( error );
+    throw new Error ( 'Error a la hora de inicializar la BD' );
+}
+
